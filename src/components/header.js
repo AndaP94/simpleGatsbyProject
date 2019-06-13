3be4337b08ed7d844e79a7090bd36934c26ea6a4
Andreas Pichler <andreaspichler@tsn.at>
2019-06-13 09:01:11 +0200

    edit header

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default props => <h1>{props.headerText}</h1>
+export default props => (<nav>
+    <h4>props.headerText</h4>
+    <ul>
+        <li><a href="/">Start</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/readme">Readme</a></li>
+    </ul>
+    </nav>)
