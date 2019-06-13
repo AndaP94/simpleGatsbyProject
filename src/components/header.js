95a43c0bb7d4abb8483c9c2810105beb426aef04
Andreas Pichler <andreaspichler@tsn.at>
2019-06-13 09:17:28 +0200

    edit header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,6 @@ import React from "react"
 export default () => (<nav>
     <h4>My frist site</h4>
     <ul>
-        <li><a href="/">Start</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/readme">Readme</a></li>
     </ul>
