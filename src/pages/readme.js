91aad5f790d2d1808f0730cda6d3e572f99053ca
Andreas Pichler <andreaspichler@tsn.at>
2019-06-13 08:34:27 +0200

    add readme.js

---
+++ src/pages/readme.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Header from "../components/header"
+export default () => (
+    <div>
+<Header headerText="About Gatsby"/>
+<div>This is a test site from andaP94</div>
+</div>)
